26b625ea9341bc1d30b3757cbf2405b316aaf0c6
AlexRuzin <stan.ruzin@gmail.com>
2024-06-28 00:44:45 -0400

    Adding another way to loop xor

--- src/encryptor.cpp
+++ src/encryptor.cpp
@@ -5,14 +5,16 @@
 #include <time.h>
 
 //definition (logic of a function)
-void xorEncrypt(char* input, uint8_t key) {
+void xorEncrypt(char *input, uint8_t key) {
+	const uint8_t inputSize = strlen(input);
+	for (uint8_t i = 0; i < inputSize; i++) {
+		input[i] ^= key;
+	}
+	
+
+
 	while (*input) {
 		*input ^= key;
 		input++;
 	}
-}
-
-
-
-
-		
+}		
